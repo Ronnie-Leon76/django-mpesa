c64697ae24934f96cb90e88c78d7e83796b2a547
Ronnie-Leon76 <ronleon76@gmail.com>
2022-04-21 07:51:14 +0300

    cast the business shortCode number

--- mpesa/LipaNaMpesaOnline.py
+++ mpesa/LipaNaMpesaOnline.py
@@ -42,38 +42,6 @@ def get_token():
     return access_token
 
 
-def check_payment_status(checkout_request_id):
-    access_token = get_token()
-    time_now = datetime.datetime.now().strftime("%Y%m%d%H%I%S")
-
-    s = shortcode + PASS_KEY + time_now
-    encoded = b64encode(s.encode('utf-8')).decode('utf-8')
-
-    api_url = "{}/mpesa/stkpushquery/v1/query".format(SAFARICOM_API)
-    headers = {
-        "Authorization": "Bearer %s" % access_token,
-        "Content-Type": "application/json",
-    }
-    request = {
-        "BusinessShortCode": shortcode,
-        "Password": encoded,
-        "Timestamp": time_now,
-        "CheckoutRequestID": checkout_request_id
-    }
-    response = requests.post(api_url, json=request, headers=headers)
-    json_response = json.loads(response.text)
-    if 'ResponseCode' in json_response and json_response["ResponseCode"] == "0":
-        result_code = json_response['ResultCode']
-        response_message = json_response['ResultDesc']
-        return {
-            "result_code": result_code,
-            "status": result_code == "0",
-            "message": response_message
-        }
-    else:
-        raise Exception("Error sending MPesa stk push", json_response)
-
-
 def sendSTK(phone_number, amount, orderId=0, transaction_id=None):
     access_token = get_token()
     time_now = datetime.datetime.now().strftime("%Y%m%d%H%I%S")
@@ -110,34 +78,47 @@ def sendSTK(phone_number, amount, orderId=0, transaction_id=None):
             transaction = PaymentTransaction.objects.filter(id=transaction_id)
             transaction.checkoutRequestID = checkoutId
             transaction.save()
-            # After creating the transaction let it also query to see if the transaction was complete
-            # If it's complete, it then updates to is_successful and isFinished
-            # This will make it easier for me to update my orders to paid
-            return check_payment_status(checkoutId)
-            # return transaction.id
+            return transaction.id
         else:
             transaction = PaymentTransaction.objects.create(phone_number=phone_number, checkoutRequestID=checkoutId,
                                                             amount=amount, order_id=orderId)
             transaction.save()
-            # After creating the transaction let it also query to see if the transaction was complete
-            # If it's complete, it then updates to is_successful and isFinished
-            # This will make it easier for me to update my orders to paid
-            # How long does it take to complete a transaction? Maybe we can set a timeout
-            return check_payment_status(checkoutId)
-            # return transaction.id
+            return transaction.id
 
     else:
         raise Exception("Error sending MPesa stk push", json_response)
 
 
-# Is this how I query from the installed Django-M-pesa library
-# from mpesa.models import PaymentTransaction
-# And performing a query to the PaymentTransaction Model
-    """def updateOrderToPaid(request, pk):
-    user = request.user
-    order = Order.objects.get(_id=pk)
-    paymentTransaction = PaymentTransaction.objects.get(order_id=str(pk))
-    # paymentTransaction = get_object_or_404(transaction,order_id=pk)
-    checkoutRequestId = paymentTransaction.checkout_request_id
-    print(check_payment_status(checkoutRequestId))
-    """
+def check_payment_status(checkout_request_id):
+    access_token = get_token()
+    time_now = datetime.datetime.now().strftime("%Y%m%d%H%I%S")
+
+    s = shortcode + PASS_KEY + time_now
+    encoded = b64encode(s.encode('utf-8')).decode('utf-8')
+
+    api_url = "{}/mpesa/stkpushquery/v1/query".format(SAFARICOM_API)
+    headers = {
+        "Authorization": "Bearer %s" % access_token,
+        "Content-Type": "application/json",
+    }
+    request = {
+        "BusinessShortCode": shortcode,
+        "Password": encoded,
+        "Timestamp": time_now,
+        "CheckoutRequestID": checkout_request_id
+    }
+    response = requests.post(api_url, json=request, headers=headers)
+    json_response = json.loads(response.text)
+    if 'ResponseCode' in json_response and json_response["ResponseCode"] == "0":
+        result_code = json_response['ResultCode']
+        response_message = json_response['ResultDesc']
+        return {
+            "result_code": result_code,
+            "status": result_code == "0",
+            "message": response_message
+        }
+    else:
+        raise Exception("Error sending MPesa stk push", json_response)
+
+
+
